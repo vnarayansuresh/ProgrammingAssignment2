30ff60916b6e859b2340220f32d88edd6e6da7b1
fearaschiarrai <feargalemurphy@gmail.com>
2016-08-02 18:17:41 +0100

    Edited cachematrix R script for Module 2 Programming Assignmnet2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## ## Note: <<- operator - can be used to assign a value to an object in an environment
+## that is different from the current environment
+## 
+## Functions to create Matrix, compute inverse (retrieve input from cache if exists)- using lexical scoping
+## First create a "matrix" (really a LIST) object 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv1 <- NULL
+    set <- function(y) {
+      x <<- y
+      inv1 <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv1 <<- inverse
+    getinverse <- function() inv1
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Now solve the inverse - use cached input data if exists 
+
+#cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  cacheSolve <- function(x, ...) {
+    inv1 <- x$getinverse()
+    if(!is.null(inv1)) {
+      message("getting cached data")
+      return(inv1)
+    }
+    matrixdata <- x$get()
+    inv1 <- solve(matrixdata, ...)
+    x$setinverse(inv1)
+    inv1
 }
